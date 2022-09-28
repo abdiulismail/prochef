ae60304a345bd48d50a8603f0d228be8a4c0bbf1
Abdullahi Ismail <79646763+abdiulismail@users.noreply.github.com>
2022-09-28 13:52:09 +0300

    adding cards

---
+++ lib/card3.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:prochef/prochef_theme.dart';
+
+class Card3 extends StatelessWidget {
+  Card3({super.key});
+
+  final String category = 'Editors choice';
+  final String title = 'the art of dough';
+  final String description = 'learn to make the perfect bread';
+  final String chef = 'abdullahi';
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        constraints: const BoxConstraints.expand(width: 350, height: 650),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/mag3.png'), fit: BoxFit.cover),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        ),
+        child: Stack(
+          children: [
+            Text(
+              category,
+              style: ProChefTheme.darkTextTheme.bodyText1,
+            ),
+            Positioned(
+                top: 20,
+                child: Text(
+                  title,
+                  style: ProChefTheme.darkTextTheme.headline2,
+                )),
+            Positioned(
+                bottom: 30,
+                right: 0,
+                child: Text(
+                  description,
+                  style: ProChefTheme.darkTextTheme.headline2,
+                )),
+            Positioned(
+              bottom: 10,
+              right: 0,
+              child: Text(
+                chef,
+                style: ProChefTheme.darkTextTheme.bodyText1,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:prochef/card1.dart';
+import 'package:prochef/card2.dart';
+import 'package:prochef/card3.dart';
+
+class Home extends StatefulWidget {
+  Home({super.key});
+
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  static List<Widget> pages = <Widget>[
+    //todo replace with card1
+    Card1(),
+    Card2(),
+    Card3()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'ProChef',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      ),
+      //todo show selected tab
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.card_giftcard), label: 'Card'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.card_giftcard), label: 'Card2'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.card_giftcard), label: 'Card3'),
+        ],
+      ),
+    );
+  }
+}
