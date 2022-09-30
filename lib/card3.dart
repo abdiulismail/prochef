704cfdc71f05b1d8b9620fce66890f61a7c3e034
Abdullahi <abdiulsheikh@gmail.com>
2022-09-30 14:09:48 +0300

    finished card3

--- lib/card3.dart
+++ lib/card3.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:prochef/prochef_theme.dart';
 
@@ -16,8 +18,155 @@ class Card3 extends StatelessWidget {
         constraints: const BoxConstraints.expand(width: 350, height: 650),
         decoration: const BoxDecoration(
           image: DecorationImage(
-              image: AssetImage('assets/mag3.png'), fit: BoxFit.cover),
+              image: AssetImage('assets/mag2.png'), fit: BoxFit.cover),
           borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        ),
+        child: Stack(
+          children: [
+            //add dark overlay boxdecoration
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.6),
+                borderRadius: BorderRadius.all(Radius.circular(10.0))
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(
+                    Icons.book,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                  SizedBox(height: 8,),
+                  Text(
+                    'Recipe Trends',
+                    style: ProChefTheme.darkTextTheme.headline6,
+                  ),
+                  SizedBox(height: 30,)
+                ],
+              ),
+            ),
+            //add center widget with chip widget children
+            Center(
+              child: Wrap(
+                alignment: WrapAlignment.start,
+                runSpacing: 10,
+                children: [
+                  Chip(
+                    label: Text('healthy',
+                    style: ProChefTheme.darkTextTheme.bodyText1,),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: (){log('deleted');},
+                  ),
+                  Chip(
+                    label: Text('vegan',
+                      style: ProChefTheme.darkTextTheme.bodyText1,),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: (){log('deleted');},
+                  ),
+                  Chip(
+                    label: Text('carrots',
+                      style: ProChefTheme.darkTextTheme.bodyText1,),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: (){log('deleted');},
+                  ),
+                  Chip(
+                    label: Text('greens',
+                      style: ProChefTheme.darkTextTheme.bodyText1,),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: (){log('deleted');},
+                  ),
+                  Chip(
+                    label: Text('wheat',
+                      style: ProChefTheme.darkTextTheme.bodyText1,),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: (){log('deleted');},
+                  ),
+                  Chip(
+                    label: Text('mangoes',
+                      style: ProChefTheme.darkTextTheme.bodyText1,),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: (){log('deleted');},
+                  ),
+                  Chip(
+                    label: Text('lemon',
+                      style: ProChefTheme.darkTextTheme.bodyText1,),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: (){log('deleted');},
+                  ),
+                  Chip(
+                    label: Text('cabbage',
+                      style: ProChefTheme.darkTextTheme.bodyText1,),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: (){log('deleted');},
+                  ),
+                  Chip(
+                    label: Text('matoke',
+                      style: ProChefTheme.darkTextTheme.bodyText1,),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: (){log('deleted');},
+                  ),
+                  Chip(
+                    label: Text('healthy',
+                      style: ProChefTheme.darkTextTheme.bodyText1,),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: (){log('deleted');},
+                  ),
+                  Chip(
+                    label: Text('vegan',
+                      style: ProChefTheme.darkTextTheme.bodyText1,),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: (){log('deleted');},
+                  ),
+                  Chip(
+                    label: Text('carrots',
+                      style: ProChefTheme.darkTextTheme.bodyText1,),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: (){log('deleted');},
+                  ),
+                  Chip(
+                    label: Text('greens',
+                      style: ProChefTheme.darkTextTheme.bodyText1,),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: (){log('deleted');},
+                  ),
+                  Chip(
+                    label: Text('wheat',
+                      style: ProChefTheme.darkTextTheme.bodyText1,),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: (){log('deleted');},
+                  ),
+                  Chip(
+                    label: Text('mangoes',
+                      style: ProChefTheme.darkTextTheme.bodyText1,),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: (){log('deleted');},
+                  ),
+                  Chip(
+                    label: Text('lemon',
+                      style: ProChefTheme.darkTextTheme.bodyText1,),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: (){log('deleted');},
+                  ),
+                  Chip(
+                    label: Text('cabbage',
+                      style: ProChefTheme.darkTextTheme.bodyText1,),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: (){log('deleted');},
+                  ),
+                  Chip(
+                    label: Text('matoke',
+                      style: ProChefTheme.darkTextTheme.bodyText1,),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: (){log('deleted');},
+                  )
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
